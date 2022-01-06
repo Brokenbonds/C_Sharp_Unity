9e6532ae5a01d0b9ba403dc9084ed3a003c0d38f
Phillip Boswell <phillip.boswell@gmail.com>
2022-01-06 01:54:18 -0800

    Project 18 and Readme Update

---
+++ Projet18 Do While True Loop and Switch/WhileTrueLoopAndSwitch/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileTrueLoopAndSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter your a name I like, Either Dave, Bob, or Martha");
+            string name = Console.ReadLine();
+            bool firstTime = false;
+            while (!firstTime)
+            {
+                switch (name)
+                {
+                    case "Dave":
+                        Console.WriteLine("Oh, nice! you choose Dave");
+                        Console.ReadLine();
+                        firstTime = true;
+                        break;
+                    case "Bob":
+                        Console.WriteLine("Oh, nice! you choose Bob");
+                        Console.ReadLine();
+                        firstTime = true;
+                        break;
+                    case "Martha":
+                        Console.WriteLine("Oh, nice! you choose Dave");
+                        Console.ReadLine();
+                        firstTime = true;
+                        break;
+                    default:
+                        Console.WriteLine("Sorry I'm going to need Dave, Bob, or Martha");
+                        name = Console.ReadLine();
+                        break;
+                }
+            }
+
+            Console.WriteLine("Now try to guess a number between 1 and 100, hint it's 12");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = userNumber == 12;
+            // this is the same as (isGuessed == false)
+
+            do
+            {
+                switch (userNumber)
+                {
+                    case 55:
+                        Console.WriteLine("you guessed 55 try again");
+                        Console.WriteLine("guess a number between 1 and 100");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 62:
+                        Console.WriteLine("you guessed 62 try again");
+                        Console.WriteLine("guess a number between 1 and 100");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("you guessed 29 try again");
+                        Console.WriteLine("guess a number between 1 and 100");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("you guessed the number 12. That is correct!");
+                        Console.ReadLine();
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Sorry,try again");
+                        Console.WriteLine("guess a number between 1 and 100");
+                        userNumber = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,5 +10,8 @@ Created a program that took in 2 peoples wage information, then spits out their 
 # Project11 Boolean Logic Submission Assignment<br>
 Created a program that assesses weather or not somone is fit for inssurance using boolean logic
 
-# Project 16 Branching Submission Assignment
+# Project 16 Branching Submission Assignment<br>
 This Program takes in User data regarding a package. Once the Weight, Height, Width, and Length of the package has been gathered it gives a quote based off these factors.
+
+# Project 18 Do While True Loop and Switch<br>
+This Program uses both a while true loop and a do while true loop. First comes the while true loop that cycles through a request for 1 of 3 names. Once that is give, it goes to the do while true loop and askes you to guess a number between 1-100 with a very good hint.
