bca3fcb43d17be200070f2e3d52bb56dd21e2758
Phillip Boswell <phillip.boswell@gmail.com>
2022-01-07 10:32:43 -0800

    Project 16 fixed issues
    Returns out of program if weight or size is to big.

--- Project16 Branching Submission Assignment/BranchingAssignment/Program.cs
+++ Project16 Branching Submission Assignment/BranchingAssignment/Program.cs
@@ -13,6 +13,15 @@ namespace BranchingAssignment
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.WriteLine("\r\nWhat is the weight of your package in lbs, please use only numbers");
             double packageWeight = Convert.ToDouble(Console.ReadLine());
+
+            if (packageWeight > 50)
+            {
+
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return;
+            }
+
             Console.WriteLine("\r\nWhat is your packages width in inches, please use only numbers");
             double packageWidth = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("\r\nWhat is your packages height in inches, please use only numbers");
@@ -20,10 +29,13 @@ namespace BranchingAssignment
             Console.WriteLine("\r\nWhat is your packages length in inches, please use only numbers");
             double packageLength = Convert.ToDouble(Console.ReadLine());
 
+            
             if (packageHeight + packageLength + packageWidth > 50)
             {
 
-                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return;
             }
             //this equation is not giving me my .00 for the cents. 
             decimal packagePricing = Convert.ToDecimal(((packageHeight * packageLength * packageWidth) * packageWeight) / 100);
