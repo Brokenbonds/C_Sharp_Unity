77e0ef213c8b94b49d09e4866b285eacca0eb652
Phillip Boswell <phillip.boswell@gmail.com>
2022-01-17 15:57:38 -0800

    Update Project 27

---
+++ Project27 Method Assignment/MethodAssignment/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Class1
+    {
+        public static int Adding26(int number)
+        {
+            number = number + 26;
+            return number;
+        }
+
+        public static int DivideBy7(int number)
+        {
+            number = number / 7;
+            return number;
+        }
+        public static int RemainderBy7(int number)
+        {
+            number = number % 7;
+            return number;
+        }
+
+        public static int MultiplyBy2(int number)
+        {
+            number = number * 2;
+            return number;
+        }
+    }
+}
--- Project27 Method Assignment/MethodAssignment/Program.cs
+++ Project27 Method Assignment/MethodAssignment/Program.cs
@@ -15,41 +15,20 @@ namespace MethodAssignment
             number = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Thank you, now we will Add 26 to " + number);
             int AddNum;
-            Console.WriteLine(AddNum = Adding26(number));
+            Console.WriteLine(AddNum = Class1.Adding26(number));
             Console.ReadLine();
             Console.WriteLine("Lets take "+number+" and divide it by 7");
-            int DivNum = DivideBy7(number);
-            int Remain = RemainderBy7(number);
+            int DivNum = Class1.DivideBy7(number);
+            int Remain = Class1.RemainderBy7(number);
             Console.WriteLine( DivNum + " with a remainder of "+Remain);
             Console.ReadLine();
             Console.WriteLine("and lastly lets multiply " + number + " by 2");
             int MultiNum;
-            Console.WriteLine(MultiNum = MultiplyBy2(number));
+            Console.WriteLine(MultiNum = Class1.MultiplyBy2(number));
             Console.ReadLine();
 
         }
 
-        public static int Adding26(int number)
-        {
-            number = number + 26;
-            return number;
-        }
-
-        public static int DivideBy7(int number)
-        {
-            number = number / 7;
-            return number;
-        }
-        public static int RemainderBy7(int number)
-        {
-            number = number % 7;
-            return number;
-        }
-
-        public static int MultiplyBy2(int number)
-        {
-            number = number * 2;
-            return number;
-        }
+        
     }
 }
