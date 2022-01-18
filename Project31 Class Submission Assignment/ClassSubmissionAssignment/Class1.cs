7f3ea2890813d0c32d11d01612a0231fb347f1a4
Phillip Boswell <phillip.boswell@gmail.com>
2022-01-18 14:37:47 -0800

    Project31 and Readme Update

---
+++ Project31 Class Submission Assignment/ClassSubmissionAssignment/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class Class1
+    {
+        public int numbers;
+        //this will take an input of x and then output x
+        public void Divideby2(int x, out int number)
+        {
+            int z = x / 2;
+            number = z;
+        }
+
+        public void OutPut(out int num, out string thing)
+        {
+            num = 10;
+            thing = "this";
+        }
+
+        public void OutPut(int me)
+        {
+            this.numbers = me;
+        }
+
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
---
+++ Project31 Class Submission Assignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Give me you name.");
+            string name = Console.ReadLine();
+            Class1.SayHi(name);
+            Console.ReadLine();
+
+            Class1 class1 = new Class1();
+            Console.WriteLine("Hey can I have a number please.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            class1.Divideby2(userNum, out int number);
+            Console.WriteLine("Thanks for the number, I'm going to divid it by 2 now");
+            Console.WriteLine(number);
+            Console.ReadLine();
+
+        }
+    }
+}
