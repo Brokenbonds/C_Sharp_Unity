3c479104d1c1d9e40082833209f292eb0bd42b4b
Phillip Boswell <phillip.boswell@gmail.com>
2022-02-02 12:09:04 -0800

    Project 33

---
+++ Project33 Abstract Class Submission/AbstractClassSubmission/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    class Employee : Person
+    {
+
+        public override void SayName()
+        {
+            Console.WriteLine("this is your name {0} {1}", firstName, lastName);
+        }
+
+        public override void SayName2()
+        {
+            base.SayName2();
+        }
+    }
+}
