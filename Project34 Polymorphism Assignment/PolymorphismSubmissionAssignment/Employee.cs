b937edd74e56adadc1911e56fd37860f7e3de1af
Phillip Boswell <phillip.boswell@gmail.com>
2022-02-02 14:04:32 -0800

    Project 34 Polymorphism

---
+++ Project34 Polymorphism Assignment/PolymorphismSubmissionAssignment/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("this is your name {0} {1}", firstName, lastName);
+        }
+
+        public override void SayName2()
+        {
+            base.SayName2();
+        }
+
+        public void Quit()
+        {
+            //exit a console application.
+            //Environment.Exit(0);
+            Console.WriteLine("I'm quitting.");
+
+        }
+    }
+}
---
+++ Project34 Polymorphism Assignment/PolymorphismSubmissionAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            IQuittable quittingTime = new Employee();
+
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+            quittingTime.Quit();
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
